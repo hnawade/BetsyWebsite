38b41aba001d98534860c49903db6e6a57134359
Harshal <harshal.nawade@gmail.com>
2019-07-19 15:29:53 -0400

    Phone verification

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -140,7 +140,7 @@ function validate(target){
 function phonenumber(inputtxt) {
     if(typeof inputtxt == "undefined")
         return;
-    var phoneno = /^\d{3}-\d{3}-\d{4}$/;
+    var phoneno = /^\d{3}-?\d{3}-?\d{4}$/;
     if((inputtxt.match(phoneno))) {
         return true;
     }
--- routes/demo.js
+++ routes/demo.js
@@ -3,23 +3,57 @@ var router = express.Router();
 
 
 router.get('/demo', function (req, res, next) {
-    console.log(req.body);
     res.render('demo', {
         title: 'Betsy Demo',
-        cellNum: ''
+        name: '',
+        email: '',
+        cellNum: '',
+        code: null
     })
 });
 
 router.post('/demo', function (req, res, next) {
     res.render('demo', {
-            title: 'Betsy Demo',
-            cellNum: req.body.tel
+        title: 'Betsy Demo',
+        name: '',
+        email: '',
+        cellNum: req.body.tel,
+        code: null
     })
 });
 
-router.post('/validate', function (req, res, next) {
-    
-})
+router.post('/validate', function(req, res, next) {
+    var AWS = require('aws-sdk');
+    AWS.config.update({region: 'us-west-2'});
 
+    console.log("Generating code");
+    var code = Math.floor(Math.random() * 9000 + 1000);
+    console.log("Code = " + code);
+    console.log(req.body);
+    console.log('+1001' + req.body.cell.replace(/\D/g, ""));
+
+    var params = {
+        Message: 'Your verification code is ' + code, /* required */
+        PhoneNumber: '+1001' + req.body.cell.replace(/\D/g, "")
+    };
+
+    var publishTextPromise = new AWS.SNS({apiVersion: '2010-03-31'}).publish(params).promise();
+
+    publishTextPromise.then(
+        function (data) {
+            console.log("MessageID is " + data.MessageId);
+        }).catch(
+        function (err) {
+            console.error(err, err.stack);
+        });
+
+    res.render('demo', {
+        title: 'Betsy Demo',
+        name: req.body.name,
+        email: req.body.email,
+        cellNum: req.body.cell,
+        code: code
+    })
+})
 
 module.exports = router;
